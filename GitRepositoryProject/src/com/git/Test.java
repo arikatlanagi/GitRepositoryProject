1d90026d8853dcf633972731b578bf822c1e2203
nagir <nagireddy0101@gmail.com>
2023-07-27 16:58:14 +0530

    First commit ....

---
+++ GitRepositoryProject/src/com/git/Test.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		boolean hungry = false;
+		if (hungry) {
+			System.out.println("Eat something !!!");
+		} else {
+			System.out.println("Do work !!!");
+		}
+
+	}
+
+}
---
+++ GitRepositoryProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nagir
+ *
+ */
+module GitRepositoryProject {
+}

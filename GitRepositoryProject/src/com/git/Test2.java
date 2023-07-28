58aea8059c35ea7cac9a84da62b29504023edf29
nagir <nagireddy0101@gmail.com>
2023-07-28 10:45:39 +0530

    committing test2 class

---
+++ GitRepositoryProject/src/com/git/Test2.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		boolean hungry = false;
+		if (hungry) {
+			System.out.println("Eat something !!!");
+		} else if (hungry == false) {
+			System.out.println("Do work !!!");
+		} else {
+			System.out.println("No inputs !!!");
+		}
+	}
+
+}
